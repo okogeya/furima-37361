5951757a3366a5f3d9c771b8a41d93b96b2bdacb
okogeya <yusannagas@gmail.com>
2022-01-18 19:57:16 +0900

    ストロングパラメーターとバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true
+  validates :first_name,       presence: true
+  validates :kana_family_name, presence: true
+  validates :kana_first_name,  presence: true
+  validates :birthday,         presence: true
 end
