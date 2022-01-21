7e6b9050d2e6c12ed5cc7614997daebb211125d8
okogeya <yusannagas@gmail.com>
2022-01-21 20:40:43 +0900

    モデルとマイグレーションファイルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :take_days
+
+  validates :item_name, presence: true
+  validates :text,      presence: true
+  validates :price,     presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :take_days_id, numericality: { other_than: 1, message: "can't be blank" } 
+end
