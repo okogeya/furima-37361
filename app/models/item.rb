8440e3fae926c82b045135ab3cee1ad344f30821
okogeya <yusannagas@gmail.com>
2022-01-25 23:20:50 +0900

    テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :text,      presence: true
   validates :price,     presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "300から9999999の間で半角入力してください" }
+  validates :image,     presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {Faker::Lorem.sentence}
+    text {Faker::Lorem.sentence}
+    category_id {"2"}
+    condition_id{"2"}
+    postage_id{"2"}
+    prefecture_id{"2"}
+    take_days_id{"2"}
+    price{"1000"}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
