44b88a574b1ed71ddbf4feff33cefff9a73cfbad
okogeya <yusannagas@gmail.com>
2022-01-26 14:58:50 +0900

    小数点入力不可のバリデーション設定（修正2回目）

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :text,      presence: true
   validates :price,     presence: true,
-                        numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '300から9999999の間で半角入力してください' }
+                        numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true, message: '300から9999999の間で半角入力してください' }
   validates :image,     presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
